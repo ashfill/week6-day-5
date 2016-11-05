c46926a845b8bf450fad0a35f3076d597d76777a
ashfill <ashtonfillery@gmail.com>
2016-11-05 12:25:49 -0700

    Web application1

---
+++ WebApplication1/WebApplication1/Models/CheckBoxItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CheckBoxItem
+    {
+        public int ID { get; set; }
+        public string Display { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class tasks
+    {
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public int UserID { get; set; }
+    }
+}
